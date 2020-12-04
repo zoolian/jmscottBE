18e7794cf3b4a02cff3425cdb6a3f3c00f826fb0
zoolian <zoolian.creative@gmail.com>
2020-12-04 13:01:54 -0600

    minor

--- SecurityManagement/src/main/java/com/jmscott/rest/SecurityManagementApplication.java
+++ SecurityManagement/src/main/java/com/jmscott/rest/SecurityManagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecurityManagementApplication {
 
+	// comment
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityManagementApplication.class, args);
 	}
