e068ee3575d4372bc6c8b54b3bd42f7dc1d17776
zoolian <zoolian.creative@gmail.com>
2020-12-05 10:39:18 -0600

    migration

--- jmscott-API/src/main/java/com/jmscott/rest/APIApplication.java
+++ jmscott-API/src/main/java/com/jmscott/rest/APIApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityManagementApplication {
+public class APIApplication {
 
-	// comment
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityManagementApplication.class, args);
+		SpringApplication.run(APIApplication.class, args);
 	}
-
 }
