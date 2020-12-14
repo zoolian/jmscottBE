e8cbd8a4d57aaf30b79abed5f4e1e716081e36b8
zoolian <zoolian.creative@gmail.com>
2020-12-14 16:04:37 -0600

    0.1.2

--- jmscott-API/src/main/java/com/jmscott/rest/APIApplication.java
+++ jmscott-API/src/main/java/com/jmscott/rest/APIApplication.java
@@ -2,9 +2,17 @@ package com.jmscott.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.logging.log4j2.SpringBootConfigurationFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class APIApplication {
+public class APIApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootConfigurationFactory.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(APIApplication.class, args);
